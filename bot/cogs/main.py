b4734518822d4c70687727ee5cb1318588f9a900
baggerfast <riosha3@gmail.com>
2022-09-06 14:15:38 +0300

    REFACTOR: naming and etc...

--- bot/cogs/main.py
+++ bot/cogs/main.py
@@ -1,15 +1,15 @@
 from nextcord.ext.commands import Bot
 
-from ds_bot.cogs.admin import register_admin_cogs
-from ds_bot.cogs.other import load_other_cogs
-from ds_bot.cogs.user import register_user_cogs
+from bot.cogs.admin import register_admin_cogs
+from bot.cogs.other import register_other_cogs
+from bot.cogs.user import register_user_cogs
 
 
 def register_all_cogs(bot: Bot) -> None:
     cogs = (
         register_user_cogs,
         register_admin_cogs,
-        load_other_cogs,
+        register_other_cogs,
     )
     for cog in cogs:
         cog(bot)
--- bot/main.py
+++ bot/main.py
@@ -1,9 +1,9 @@
 from nextcord import Intents
 from nextcord.ext.commands import Bot
 
-from ds_bot.misc import Env, Config
-from ds_bot.cogs import register_all_cogs
-from ds_bot.database.models import register_models
+from bot.misc import Env, Config
+from bot.cogs import register_all_cogs
+from bot.database.models import register_models
 
 
 def start_bot():
